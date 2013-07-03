863bf36542ef427705d3d165b1c85cfc2d186e71
Björn Persson <bjorn@cedur.se>
2013-07-03 19:55:25 +0200

    TestCustomer

--- MovieStore/MovieStore/UnitTest1.cs
+++ MovieStore/MovieStore/UnitTest1.cs
@@ -8,22 +8,25 @@ namespace MovieStore
     {
 
         // Movies
-        Movie m_Cinderella;
-        Movie m_StarWars;
-        Movie m_Gladiator;
-        Movie m_BruceAlmighty;
+        private Movie m_Cinderella;
+        private Movie m_StarWars;
+        private Movie m_Gladiator;
+        private Movie m_BruceAlmighty;
+
         // Rentals
-        Rental m_Rental1;
-        Rental m_Rental2;
-        Rental m_Rental3;
-        Rental m_Rental4;
+        private Rental m_Rental1;
+        private Rental m_Rental2;
+        private Rental m_Rental3;
+        private Rental m_Rental4;
+
         // Customers
-        Customer m_MickeyMouse;
-        Customer m_DonaldDuck;
-        Customer m_BillClinton;
-        Customer m_GeorgeBush;
+        private Customer m_MickeyMouse;
+        private Customer m_DonaldDuck;
+        private Customer m_BillClinton;
+        private Customer m_GeorgeBush;
 
         protected bool IsRunInSession { get; set; }
+
         [TestInitialize]
         public void Startup()
         {
@@ -78,6 +81,55 @@ namespace MovieStore
             Assert.AreEqual(5, m_Rental1.DaysRented);
             Assert.AreEqual(5, m_Rental1.DaysRented);
             Assert.AreEqual(5, m_Rental1.DaysRented);
+        }
+
+        [TestMethod]
+        public void TestCustomer()
+        {
+            // Test Name property
+            Assert.AreEqual("Mickey Mouse", m_MickeyMouse.Name);
+            Assert.AreEqual("Donald Duck", m_DonaldDuck.Name);
+            Assert.AreEqual("Bill Clinton", m_BillClinton.Name);
+            // Test AddRental() method - set up for test
+            m_MickeyMouse.AddRental(m_Rental1);
+            m_MickeyMouse.AddRental(m_Rental2);
+            m_MickeyMouse.AddRental(m_Rental3);
+            /* At this point, the structure of the program begins getting in the
+            * way of testing. Rentals are imbedded in the Customer object, but
+            * there is no property to access them. They can only be accessed
+            * internally, by the Statement() method, which imbeds them in the
+            * text string passed as it's return value. So, to get these amounts,
+            * we will have to parse that value. */
+            // Test the Statement() method
+            string theResult = m_MickeyMouse.Statement();
+            // Parse the result
+            char[] delimiters = "\n\t".ToCharArray();
+            string[] results = theResult.Split(delimiters);
+            /* The results[] array will have the following elements:
+            * [0] = junk
+            * [1] = junk
+            * [2] = title #1
+            * [3] = price #1
+            * [4] = junk
+            * [5] = title #2
+            * [6] = price #2
+            * [7] = junk
+            * [8] = title #3
+            * [9] = price #3
+            * [10] = "Amount owed is x"
+            * [11] = "You earned x frequent renter points."
+            * We will test the title and price elements, and the total
+            * and frequent renter points items. If these tests pass, then
+            * we know that AddRentals() is adding rentals to a Customer
+            * object properly, and that the Statement() method is
+            * generating a statement in the expected format. */
+            // Test the title and price items
+            Assert.AreEqual("Cinderella", results[2]);
+            Assert.AreEqual(3, Convert.ToDouble(results[3]));
+            Assert.AreEqual("Star Wars", results[5]);
+            Assert.AreEqual(6.5, Convert.ToDouble(results[6]));
+            Assert.AreEqual("Gladiator", results[8]);
+            Assert.AreEqual(15, Convert.ToDouble(results[9]));
         }
     }
 }
