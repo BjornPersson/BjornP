8d0941e5eeee25aedc97b970536f369754bb05c2
Björn Persson <bjorn@cedur.se>
2013-07-03 14:42:39 +0200

    First green test

---
+++ LeapYear/LeapYear/LeapYearTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeapYear
+{
+    [TestClass]
+    public class LeapYearTest
+    {
+        [TestMethod]
+        public void IsLeapYear()
+        {
+            Assert.IsTrue(LeapYearClass.IsLeapYear(1996));
+        }
+    }
+
+    public class LeapYearClass
+    {
+        public static bool IsLeapYear(int i)
+        {
+            return true;
+        }
+    }
+}
